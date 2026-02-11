47bc4d3179362a0ef13aaa8b7fd41f216b502f83
vabjo <valabjornsd@gmail.com>
2026-02-11 13:36:06 +0100

    Problem: Behövde klicka två gånger för att aktivera click-event Lösning: tomt värde i style.display ist för "none"

--- Laboration-1/src/js/main.js
+++ Laboration-1/src/js/main.js
@@ -1,11 +1,29 @@
 /*Program som visar en bild när knappeen på index sidan klickas*/
-let button = document.querySelector(".btn");
-let jam = document.querySelector(".jammed");
+"use strict";
 
-button.addEventListener("click", () => {
-    if (jam.style.display === "none") {
-        jam.style.display = "block";
-    } else {
-        jam.style.display = "none";
-    }
-});
+window.onload = getJammed;
+
+function getJammed() {
+    let button = document.querySelector(".btn");
+    let jam = document.querySelector(".jammed");
+
+    button.addEventListener("click", () => {
+        if (jam.style.display === "") {
+            jam.style.display = "block";
+        } else {
+            jam.style.display = "";
+        }
+    });
+}
+
+/*TIDIGARE IF-STATEMENT:
+
+ if (jam.style.display === "none") {
+            jam.style.display = "block";
+        } else {
+            jam.style.display = "none";
+        }
+
+Problem: Behövde klicka två gånger på knappen för att aktivera click-event
+Lösning: Genom att ge style.display ett tomt värde istället för "none" aktiveras eventet vid första klick
+ */
