f90c235706700e3ce511fb9864bd5e23411032ca
vabjo <valabjornsd@gmail.com>
2026-02-11 18:25:13 +0100

    Felsök test 1.4.4

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: { /* Lägger till de html filer som ska användas */
-                main: resolve(__dirname, "/index.html"),
-                photos: resolve(__dirname, "/photos.html"), /* bäst att döpa samma namn om inte main, annars strul att hitta */
-                process: resolve(__dirname, "/process.html")
+                main: resolve(__dirname, "index.html"),
+                photos: resolve(__dirname, "photos.html"), /* bäst att döpa samma namn om inte main, annars strul att hitta */
+                process: resolve(__dirname, "process.html")
             }
         }
     },
